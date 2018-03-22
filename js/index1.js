d3836f8e31c3e22eafaa9e1fbd8df7e5b312bf1c
deepikacs <dshivalingegowda@qwinix.io>
2018-03-22 16:44:34 +0530

    changes

---
+++ js/index1.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function(){
+	debugger;
+	$(".about1").hide();
+    $("#home").click(function(){
+    	debugger;
+        $(".about1").hide();
+        $(".home1").show();
+    });
+   //  $("#about").click(function(){
+   //  	debugger;
+   //      $(".home1").hide();
+   //      $(".about1").show();
+   //  });
+   //  $("#info2").click(function(){
+   //      debugger
+   //     $(".home1").hide();
+   //      $(".about1").show();
+   // });
+
+
+});
+    function showAbout() {
+        $(".home1").hide();
+        $(".about1").show();
+        // body...
+    }
+// @media only screen and (min-width : 320px){
+//         .cs{color:blue;
+//            font-size:20px;}
+ 
+//     }
+ 
+//     /* Extra Small Devices, Phones */
+//     @media only screen and (min-width : 480px){
+//      .cs{color:blue;
+//            font-size:20px;}
+//     }
+ 
+//     /* Small Devices, Tablets */
+//     @media only screen and (min-width : 768px){
+//      .cs{color:blue;
+//            font-size:20px;}
+//     }
+ 
+//     /* Medium Devices, Desktops */
+//     @media only screen and (min-width : 992px){
+//      .cs{color:blue;
+//            font-size:20px;}
+//     }
+ 
+//     /* Large Devices, Wide Screens */
+//     @media only screen and (min-width : 1200px){
+//      .cs{color:blue;
+//            font-size:20px;}
+//     }
