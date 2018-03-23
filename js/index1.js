92f397bf7dee1930676eb3e4e36f489253286ba8
deepikacs <dshivalingegowda@qwinix.io>
2018-03-23 14:31:29 +0530

    changes1

--- js/index1.js
+++ js/index1.js
@@ -1,56 +1,68 @@
 
 $(document).ready(function(){
-	debugger;
+
 	$(".about1").hide();
     $("#home").click(function(){
     	debugger;
         $(".about1").hide();
         $(".home1").show();
     });
-   //  $("#about").click(function(){
-   //  	debugger;
-   //      $(".home1").hide();
-   //      $(".about1").show();
-   //  });
-   //  $("#info2").click(function(){
-   //      debugger
-   //     $(".home1").hide();
-   //      $(".about1").show();
-   // });
-
-
-});
-    function showAbout() {
+     $("#about").click(function(){
+ 	debugger;
         $(".home1").hide();
         $(".about1").show();
-        // body...
-    }
-// @media only screen and (min-width : 320px){
-//         .cs{color:blue;
-//            font-size:20px;}
- 
-//     }
- 
-//     /* Extra Small Devices, Phones */
-//     @media only screen and (min-width : 480px){
-//      .cs{color:blue;
-//            font-size:20px;}
-//     }
- 
-//     /* Small Devices, Tablets */
-//     @media only screen and (min-width : 768px){
-//      .cs{color:blue;
-//            font-size:20px;}
-//     }
- 
-//     /* Medium Devices, Desktops */
-//     @media only screen and (min-width : 992px){
-//      .cs{color:blue;
-//            font-size:20px;}
-//     }
- 
-//     /* Large Devices, Wide Screens */
-//     @media only screen and (min-width : 1200px){
-//      .cs{color:blue;
-//            font-size:20px;}
-//     }
+    });
+     debugger;
+    $("#info").click(function(){
+        debugger
+       $(".home1").hide();
+        $(".about1").show();
+   });
+     $("#info1").click(function(){
+        debugger
+       $(".home1").hide();
+        $(".about1").show();
+   });
+      $("#info2").click(function(){
+        debugger
+       $(".home1").hide();
+        $(".about1").show();
+   });
+    $("#more").click(function(){
+        debugger
+       $(".home1").hide();
+        $(".about1").show();
+   });
+   
+   // function showAbout() {
+   //  debugger;
+   //  $(".home1").hide();
+   //  $(".about1").show();
+   //      // body...
+   //  }
+// 
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 100) {
+    //         $('.scrollup').fadeIn();
+    //     } else {
+    //         $('.scrollup').fadeOut();
+    //     }
+    // });
+
+    // $('.scrollup').click(function () {
+    //     $("html, body").animate({
+    //         scrollTop: 0
+    //     }, 600);
+    //     return false;
+    // });
+    $(".scrollup").click(function() {
+        debugger;
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+    // $("#showAbout").click(function(){
+        // debugger
+    // })
+
+});
